7933bc62e7aecce5daa3dc24993d0a14ac59d88f
BigMoistLochu <krok.konrad1@gmail.com>
2023-12-05 02:42:09 +0100

    add project to remote repo

---
+++ src/main/java/com/example/tibiaeventbusproject/TibiaEventBusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tibiaeventbusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibiaEventBusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TibiaEventBusProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tibiaeventbusproject/TibiaEventBusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tibiaeventbusproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaEventBusProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
