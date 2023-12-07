249c620a872ea4af701b80fd490593566568797f
BigMoistLochu <krok.konrad1@gmail.com>
2023-12-07 21:22:35 +0100

    add TibiaCharacterController give resources to front

---
+++ src/main/java/com/example/tibiaeventbusproject/restApi/TibiaCharacterController.java
@@ -0,0 +1,11 @@
+package com.example.tibiaeventbusproject.restApi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TibiaCharacterController {
+
+
+
+
+}
