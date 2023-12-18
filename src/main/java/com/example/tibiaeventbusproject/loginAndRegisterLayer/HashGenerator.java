c17a1927ec8e3517b18fde81e104dcbab2fe5964
BigMoistLochu <krok.konrad1@gmail.com>
2023-12-18 20:42:30 +0100

    add methods to HashGenerator, one is responsible for generete the hash, and one check the checka and return boolean

--- src/main/java/com/example/tibiaeventbusproject/loginAndRegisterLayer/HashGenerator.java
+++ src/main/java/com/example/tibiaeventbusproject/loginAndRegisterLayer/HashGenerator.java
@@ -1,4 +1,34 @@
 package com.example.tibiaeventbusproject.loginAndRegisterLayer;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public class HashGenerator {
+
+
+    //potrzebujesz HashGenerator oraz HashVeryfikator
+
+    //chcesz uzywac tej funkcji tylko do hashowania wiec nie potrzebujesz kilku instancji
+
+    public String getBcryptHashString(String email)
+    {
+        //$2a$12$US00g/uMhoSBm.HiuieBjeMtoN69SN.GE25fCpldebzkryUyopws6 przykladowy hash
+        if(email instanceof String && email!=null)
+        {
+            String bcryptHashString = BCrypt.withDefaults().hashToString(12, email.toCharArray());
+            return bcryptHashString;
+        }
+        return null;
+    }
+
+    public boolean verifyBcryptHashString(String email,String bcryptHashString)
+    {
+        BCrypt.Result result = BCrypt.verifyer().verify(email.toCharArray(), bcryptHashString);
+        return result.verified;
+    }
+
+
+
+
+
+
 }
