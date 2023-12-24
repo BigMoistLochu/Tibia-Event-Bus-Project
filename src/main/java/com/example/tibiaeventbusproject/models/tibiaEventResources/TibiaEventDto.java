a32bfa89e5a0f613d0cb3b13c24e2990a499851b
BigMoistLochu <krok.konrad1@gmail.com>
2023-12-24 12:11:43 +0100

    i added hashcode and equels method to TibiaEventDto, tested TibiaEventJsonHandler

--- src/main/java/com/example/tibiaeventbusproject/models/tibiaEventResources/TibiaEventDto.java
+++ src/main/java/com/example/tibiaeventbusproject/models/tibiaEventResources/TibiaEventDto.java
@@ -3,6 +3,8 @@ package com.example.tibiaeventbusproject.models.tibiaEventResources;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 public class TibiaEventDto {
 
     @JsonProperty("nick")
@@ -19,6 +21,12 @@ public class TibiaEventDto {
 
     public TibiaEventDto(){}
 
+    public TibiaEventDto(String nick, String state, String time, String hashAuth) {
+        this.nick = nick;
+        this.state = state;
+        this.time = time;
+        this.hashAuth = hashAuth;
+    }
 
     public String getState() {
         return state;
@@ -50,6 +58,18 @@ public class TibiaEventDto {
 
     public void setHashAuth(String hashAuth) {
         this.hashAuth = hashAuth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TibiaEventDto that)) return false;
+        return Objects.equals(getNick(), that.getNick()) && Objects.equals(getState(), that.getState()) && Objects.equals(getTime(), that.getTime()) && Objects.equals(getHashAuth(), that.getHashAuth());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNick(), getState(), getTime(), getHashAuth());
     }
 
     @Override
--- src/test/java/com/example/tibiaeventbusproject/discordWebHookLayer/TibiaEventJsonHandlerTest.java
+++ src/test/java/com/example/tibiaeventbusproject/discordWebHookLayer/TibiaEventJsonHandlerTest.java
@@ -1,32 +1,40 @@
 package com.example.tibiaeventbusproject.discordWebHookLayer;
 
+import com.example.tibiaeventbusproject.models.tibiaEventResources.TibiaEventDto;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TibiaEventJsonHandlerTest {
-    //if(doesMessageContainBotOTCMessage(message))
-    //        {
-    //            String json = extractJsonFromMessage(message);
-    //            if(json!=null)
-    //            {
-    //                TibiaEventDto tibiaEventDto = getEventFromJson(json);
-    //                if(tibiaEventDto !=null)
-    //                {
-    //                    return tibiaEventDto;
-    //                }
-    //            }
-    //        }
-    //        return null;
 
-    @BeforeEach
-    void setUp() {
+
+    @Test
+    void getParsedTibiaEventShouldReturnNull() {
+
+        // 1. Przygotowanie danych testowych
+        String wrongMessage = "bototc.messag({\"nick\" : \"Masszny\",\"state\" : \"Frag\",\"time\" : \"2023/12/13 05:12:45\",\"hash\" : \"81ncasdkdam\"})";
+        // 2. Wywołanie metody
+        TibiaEventDto event = TibiaEventJsonHandler.getParsedTibiaEvent(wrongMessage);
+        //3. Sprawdzenie wyników
+        assertNull(event);
+
     }
 
     @Test
-    void getParsedTibiaEvent() {
+    void getParsedTibiaEventShouldReturnTibiaEventDto() {
 
+        // 1. Przygotowanie danych testowych
+        String correctJson = "bototc.message({\"nick\" : \"Masszny\",\"state\" : \"Frag\",\"time\" : \"2023/12/13 05:12:45\",\"hash\" : \"81ncasdkdam\"})";
+        TibiaEventDto tibiaEventDtoByContructor = new TibiaEventDto("Masszny","Frag","2023/12/13 05:12:45","81ncasdkdam");
+        // 2. Wywołanie metody
+        TibiaEventDto tibiaEventFromMethod = TibiaEventJsonHandler.getParsedTibiaEvent(correctJson);
+        //3. Sprawdzenie wyników
+        assertEquals(tibiaEventFromMethod,tibiaEventDtoByContructor);
 
     }
 }
