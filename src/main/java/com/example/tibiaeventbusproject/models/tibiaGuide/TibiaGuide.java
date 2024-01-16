9ba61e0eef0eab1ae490106aa16219f00ff5df9d
BigMoistLochu <krok.konrad1@gmail.com>
2024-01-16 18:39:18 +0100

    added TibiaGuide

--- src/main/java/com/example/tibiaeventbusproject/models/tibiaGuide/TibiaGuide.java
+++ src/main/java/com/example/tibiaeventbusproject/models/tibiaGuide/TibiaGuide.java
@@ -1,4 +1,37 @@
 package com.example.tibiaeventbusproject.models.tibiaGuide;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class TibiaGuide {
+
+
+    private String name;
+
+    private String description;
+
+
+    public TibiaGuide() {
+    }
+
+    public TibiaGuide(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
