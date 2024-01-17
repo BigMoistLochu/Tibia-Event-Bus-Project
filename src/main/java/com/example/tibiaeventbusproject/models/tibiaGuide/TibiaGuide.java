0627b6c0ea460e492bd54ece1b8fd49cb481002a
BigMoistLochu <krok.konrad1@gmail.com>
2024-01-17 23:38:57 +0100

    added TibiaGuide id

--- src/main/java/com/example/tibiaeventbusproject/models/tibiaGuide/TibiaGuide.java
+++ src/main/java/com/example/tibiaeventbusproject/models/tibiaGuide/TibiaGuide.java
@@ -1,11 +1,13 @@
 package com.example.tibiaeventbusproject.models.tibiaGuide;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class TibiaGuide {
 
-
+    @Id
+    private Long id;
     private String name;
 
     private String description;
